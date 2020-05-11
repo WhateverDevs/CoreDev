f6d587e90a28de6ad7094a7534cbfcf1b9c67c4c
Rodrigo Varga <varguiniano@gmail.com>
2020-05-11 17:05:16 +0200

    - Some simple Ui utils.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,8 @@
+﻿using WhateverDevs.Core.Runtime.Ui;
+
+public class Test : ActionOnButtonClick<Test>
+{
+    
+
+    protected override void ButtonClicked() => throw new System.NotImplementedException();
+}
