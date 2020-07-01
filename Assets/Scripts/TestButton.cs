90b0bdfe60e9d4a896641d1c3b85cdf9efdefb83
Rodrigo Varga <varguiniano@gmail.com>
2020-07-01 13:00:16 +0200

    - Updated logger.
    - Textmesh pro.

---
+++ Assets/Scripts/TestButton.cs
@@ -0,0 +1,8 @@
+﻿using WhateverDevs.Core.Runtime.Ui;
+
+public class TestButton : ActionOnButtonClick<TestButton>
+{
+    public Test Tester;
+
+    protected override void ButtonClicked() => Tester.Log();
+}
