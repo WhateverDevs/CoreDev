5fdb9080083c5f5e322e67c16ee10c0e255e3ffd
Rodrigo Varga <rodrigo.varga@itcl.es>
2020-11-26 10:22:53 +0100

    - Added logging of exceptions to file and socket.

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,8 +1,16 @@
-﻿using Sirenix.OdinInspector;
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
 using WhateverDevs.Core.Runtime.Common;
 
 public class Test : LoggableMonoBehaviour<Test>
 {
+    public Renderer Empty;
+    
     [Button]
-    public void Log() => GetLogger().Info("Test");
+    public void Log()
+    {
+        GetLogger().Info("Test");
+        GetLogger().Info(Empty.enabled);
+    }
 }
