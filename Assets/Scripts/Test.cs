53d5d14e4ac8d22537fab780569d864b21f7f4f7
Rodrigo Varga <rodrigo.varga@itcl.es>
2021-07-22 13:03:47 +0200

    - Updating packages.

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,46 +1,15 @@
-﻿using System;
-using Sirenix.OdinInspector;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using WhateverDevs.Core.Runtime.Common;
+﻿using WhateverDevs.Core.Runtime.Common;
 using WhateverDevs.Core.Runtime.DataStructures;
-using WhateverDevs.SceneManagement.Runtime.SceneManagement;
-using WhateverDevs.TwoDAudio.Runtime;
-using Zenject;
 
 public class Test : LoggableMonoBehaviour<Test>
 {
-    public SerializableDictionary<LoadSceneMode, GameObject> SerializableDictionary =
-        new SerializableDictionary<LoadSceneMode, GameObject>();
-
-    public Renderer Empty;
-
-    public PositionData SpawnPosition;
-
-    public AudioReference Audio;
-
-    public SceneReference Scene;
-
-    [Inject]
-    public IAudioManager AudioManager;
-
-    [Inject]
-    public IAudioLibrary AudioLibrary;
+    public Tag Tag;
 
     private void OnEnable()
     {
-        Log();
-    }
-
-    [Button]
-    public void Log()
-    {
-        AudioManager.PlayAudio(Audio);
-        AudioManager.PlayAudio(Audio);
-        AudioManager.PlayAudio(Audio);
-        AudioManager.PlayAudio(Audio);
-        AudioManager.PlayAudio(Audio);
-
-        Logger.Error("Fuck!");
+        string someShit = Tag;
+        Tag = someShit;
+        Logger.Info(someShit);
+        Logger.Info(Tag);
     }
 }
--- Assets/Scripts/TestButton.cs
+++
@@ -1,8 +0,0 @@
-﻿using WhateverDevs.Core.Runtime.Ui;
-
-public class TestButton : ActionOnButtonClick<TestButton>
-{
-    public Test Tester;
-
-    protected override void ButtonClicked() => Tester.Log();
-}
