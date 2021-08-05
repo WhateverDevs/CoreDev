c8dffc3de7665531df0a93230610bdd0454a14d9
Rodrigo Varga <rodrigo.varga@itcl.es>
2021-08-05 08:55:56 +0200

    - Updated modules.

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,13 +3,5 @@ using WhateverDevs.Core.Runtime.DataStructures;
 
 public class Test : LoggableMonoBehaviour<Test>
 {
-    public Tag Tag;
-
-    private void OnEnable()
-    {
-        string someShit = Tag;
-        Tag = someShit;
-        Logger.Info(someShit);
-        Logger.Info(Tag);
-    }
+    public PositionData PositionData;
 }
