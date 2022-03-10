bc3c5b29b19eee585fc9c5f2784a00af8f3d5026
Varga <varguiniano@gmail.com>
2022-03-10 10:03:14 +0100

    - Audio reference drawer replaced by Odin Attributes.

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,7 +1,10 @@
 ﻿using WhateverDevs.Core.Runtime.Common;
 using WhateverDevs.Core.Runtime.DataStructures;
+using WhateverDevs.TwoDAudio.Runtime;
 
 public class Test : LoggableMonoBehaviour<Test>
 {
     public PositionData PositionData;
+
+    public AudioReference AudioTest;
 }
