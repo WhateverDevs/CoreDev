6586d758e176f79a7daf53ee910303f9bc65b9e5
Rodrigo Varga <varguiniano@gmail.com>
2023-02-07 08:23:37 +0100

    - Implemented a way to run hooks just before playing the app.
    - Updated to Unity 2022.2 for easier dev but didn't bump version requirements.

---
+++ Assets/Editor/TestPlayHook.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using WhateverDevs.DefaultToolBarButtons.Editor;
+
+namespace Editor
+{
+    [CreateAssetMenu(menuName = "Test/PlayHook", fileName = "TestPlayHook")]
+    public class TestPlayHook : PlayHook
+    {
+        // This is very stupid because the console usually clears on play but works as an example.
+        public override void Run() => Logger.Info("Running before play.");
+    }
+}
